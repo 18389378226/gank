dfdb0edaf67fa4db95e8ed566a75634ff1b55343
18389378226 <smashing_chen@163.com>
2016-11-29 16:42:27 +0800

    第一次提交代码

---
+++ app/src/main/java/com/chensen/gank/common/Constants.java
@@ -0,0 +1,11 @@
+package com.chensen.gank.common;
+
+/**
+ * author：chensen on 2016/11/25 14:22
+ * desc：
+ */
+
+public class Constants {
+    public static final String PATH_CACHE = "http-cache";
+    public static final String GIRL_URL ="girl_url";
+}
